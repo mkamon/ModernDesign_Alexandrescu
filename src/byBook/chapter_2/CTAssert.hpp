b4151dd94aa4751101885fdefa351db5741279bd
mkamon <mich.kamon@gmail.com>
2021-06-15 10:06:14 +0200

    [AA.2.1] Add safe reinterpret cast with messaage

--- src/byBook/chapter_2/CTAssert.hpp
+++ src/byBook/chapter_2/CTAssert.hpp
@@ -1,10 +1,34 @@
 #pragma once
 
+
+    template<bool>
+    struct CompileTimeChecker
+    {
+        CompileTimeChecker(...);
+    };
+
+    template<> struct CompileTimeChecker<false> {};
+
+    #define STATIC_CHECK(expr, msg) \
+        {\
+            class ERROR_##msg {}; \
+            (void)sizeof(CompileTimeChecker< (expr) != 0 >(( ERROR_##msg() )));\
+        }
+
 namespace ctassert
 {
+
     template<typename To, typename From>
     To safe_reinterpret_cast(From from) {
         static_assert(sizeof(From) <= sizeof(To));
         return reinterpret_cast<To>(from);
     }
+
+    template<typename To, typename From>
+    To safe_reinterpret_cast_with_msg(From from) {
+        STATIC_CHECK(sizeof(From) <= sizeof(To),
+            Destitation_Type_Too_Narrow);
+        return reinterpret_cast<To>(from);
+    }
+    
 }
--- ut/chapter_2/testCTAssert.cpp
+++ ut/chapter_2/testCTAssert.cpp
@@ -29,3 +29,29 @@ TEST(CompileTimeAssert, SafeReinterpretCompilationTest)
 //    FAIL() << "Compilation should not succeed";
 //}
 //
+
+TEST(CompileTimeAssert, SafeReinterpretWithMessageCompilationTest)
+{
+    //given
+    using From = unsigned;
+    using To = unsigned char;
+    auto num = std::numeric_limits<From>::max();
+    //when
+    auto p = ctassert::safe_reinterpret_cast_with_msg<To *>(&num);
+    //then
+    for(From i=0; i<sizeof(num)/sizeof(To); i++){
+        ASSERT_EQ(*(p+i), std::numeric_limits<To>::max());
+    }
+}
+
+// TEST(CompileTimeAssert, SafeReinterpretWithMessageShouldNotCompileTest)
+// {
+//    //given
+//    using From = unsigned;
+//    using To = unsigned char;
+//    From num = std::numeric_limits<From>::max();
+//    //when
+//    To p = ctassert::safe_reinterpret_cast_with_msg<To>(num);
+//    //then
+//    FAIL() << "Compilation should not succeed";
+// }
