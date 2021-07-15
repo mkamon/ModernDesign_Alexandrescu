d745eae3dc7b659c05d54ee6b55d24cd462e6132
mkamon <mich.kamon@gmail.com>
2021-07-15 07:45:58 +0200

    [AA-3.9] Add accessing scattered hierarchy by index
    The following commit provides an implementation of index-based access to
    types in scatter hierarchy. The implementation is based on Lokie's
    HierarchyGenerators.h since it diverged from the book since it's release.

--- src/byBook/chapter_3/ScatteredHierarchy.hpp
+++ src/byBook/chapter_3/ScatteredHierarchy.hpp
@@ -1,26 +1,113 @@
 #pragma once
 #include "chapter_2/NullType.hpp"
 #include "chapter_3/TypeList.hpp"
+#include "chapter_2/MapToType.hpp"
+
+namespace Private 
+{
+    template <typename, typename>
+    struct ScatterHierarchyTag;
+}
 
 template <typename TList, template <typename> typename Unit>
 class GenScatterHierarchy;
 
 template <typename T1, typename T2, template <typename> typename Unit>
 class GenScatterHierarchy<TypeList<T1,T2>, Unit>
-    : public GenScatterHierarchy<T1, Unit>
+    : public GenScatterHierarchy<Private::ScatterHierarchyTag<T1, T2>, Unit>
     , public GenScatterHierarchy<T2, Unit>
 {
+public:
     using TList = TypeList<T1, T2>;
-    using LeftBase = GenScatterHierarchy<T1, Unit>;
+    using LeftBase = GenScatterHierarchy<Private::ScatterHierarchyTag<T1, T2>, Unit>;
     using RightBase = GenScatterHierarchy<T2, Unit>;
+    template <typename T> 
+    struct Rebind
+    {
+        using Result = Unit<T>;
+    };
 };
+
+template <typename T1, typename T2, template <typename> typename Unit>
+class GenScatterHierarchy<Private::ScatterHierarchyTag<T1, T2>, Unit>
+    : public GenScatterHierarchy<T1, Unit>
+{};
 
 template <typename NonListType, template <typename> typename Unit>
 class GenScatterHierarchy : public Unit<NonListType>
 {
     using LeftBase = Unit<NonListType>;
+
+    template <typename T> 
+    struct Rebind
+    {
+        using Result = Unit<T>;
+    };
 } ;
 
 template <template <typename> typename Unit>
 class GenScatterHierarchy<NullType, Unit>
-{};
+{
+    template <typename T> 
+    struct Rebind
+    {
+        using Result = Unit<T>;
+    };
+};
+
+// Field access to a object genrated wiht GenScatteredHierarchy
+
+template <typename T, typename H>
+typename H::template Rebind<T>::Result &Field(H &scatterObj)
+{
+    return scatterObj;
+};
+
+// template <typename T, typename H>
+// const typename H::template Rebind<T>::Result &Field(const H &scatterObj)
+// {
+//     return scatterObj;
+// };
+
+template <typename H, unsigned int i>
+struct FieldHelper;
+
+template <typename H>
+struct FieldHelper<H, 0>
+{
+    using ElementType = typename H::TList::Head;
+    using UnitType = typename H::template Rebind<ElementType>::Result;
+    using LeftBase = typename H::LeftBase;
+    
+    static UnitType &Do(H &obj)
+    {
+        LeftBase & leftBase = obj;
+        return leftBase;
+    };
+};
+
+template <typename H, unsigned int i>
+struct FieldHelper
+{
+    using ElementType = typename TL::TypeAt<typename H::TList, i>::Result;
+    using UnitType = typename H::template Rebind<ElementType>::Result;
+    using RightBase = typename H::RightBase;
+
+    static UnitType &Do(H &obj)
+    {
+        RightBase &rightBase = obj;
+        return FieldHelper<RightBase, i - 1>::Do(rightBase);
+    };
+};
+
+template <int i, typename H>
+typename FieldHelper<H, i>::UnitType &
+Field(H &obj)
+{
+    return FieldHelper<H, i>::Do(obj);
+};
+
+
+
+
+
--- ut/chapter_3/testGenScatterHierarchy.cpp
+++ ut/chapter_3/testGenScatterHierarchy.cpp
@@ -12,15 +12,44 @@ namespace ScatterTest
     };
 }
 
-TEST(ScatterHierarchyTest, examleScatterHierarchyGenerationTest)
+TEST(ScatterHierarchyTest, scatterHierarchyGenerationTest)
 {
     //given
     using TestTypes = TYPELIST_3(int, std::string, widgets::ConcreteWidget);
     //when
-    GenScatterHierarchy<TestTypes, ScatterTest::Holder> bigHolder;
+    using ScatteredHolder = GenScatterHierarchy<TestTypes, ScatterTest::Holder>;
+    ScatteredHolder bigHolder;
     //then 
-    ASSERT_TRUE(static_cast<ScatterTest::Holder<std::string>&>(bigHolder).value.empty() );
+    ASSERT_TRUE(static_cast<ScatterTest::Holder<std::string>&>(bigHolder).value.empty());
     ASSERT_EQ(widgets::defaultWidgetXValue, static_cast<ScatterTest::Holder<widgets::ConcreteWidget>&>(bigHolder).value.getX());
 }
 
+TEST(ScatterHierarchyTest, accesingScatteredHierarchyByFieldType)
+{
+    //given
+    using TestTypes = TYPELIST_3(int, std::string, widgets::ConcreteWidget);
+    //when
+    using ScatteredHolder = GenScatterHierarchy<TestTypes, ScatterTest::Holder>;
+    ScatteredHolder bigHolder;
+    auto &stringValue = Field<std::string>(bigHolder).value; 
+    auto &widgetValue = Field<widgets::ConcreteWidget>(bigHolder).value;
+    //then 
+    ASSERT_TRUE(stringValue.empty());
+    ASSERT_EQ(widgets::defaultWidgetXValue, widgetValue.getX());
+}
 
+TEST(ScatterHierarchyTest, accessingScatteredHierarcyByIndex)
+{
+    //given
+    using TestTypes = TYPELIST_3(int, std::string, widgets::ConcreteWidget);
+    constexpr int stringIndex = 1;
+    constexpr int widgetIndex = 2;
+    //when
+    using ScatteredHolder = GenScatterHierarchy<TestTypes, ScatterTest::Holder>;
+    ScatteredHolder bigHolder;
+    auto &stringValue = Field<stringIndex>(bigHolder).value; 
+    auto &widgetValue = Field<widgetIndex>(bigHolder).value;
+    //then 
+    ASSERT_TRUE(stringValue.empty());
+    ASSERT_EQ(widgets::defaultWidgetXValue, widgetValue.getX()); 
+}
