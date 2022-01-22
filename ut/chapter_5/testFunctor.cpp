09e5f5a7223242d1d8010b83485e3b4958db9a40
mkamon <mich.kamon@gmail.com>
2022-01-22 12:30:33 +0100

    [AA-5.3] Functor with functions test added
    The commit provides a tests for constructing and executing
    Functors given a function pointer. The tests covers the case
    with function overload, when the explict cast is needed for
    function's type deduction.

--- ut/chapter_5/testFunctor.cpp
+++ ut/chapter_5/testFunctor.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include "chapter_5/Functor.hpp"
 
+namespace testing
+{
+
 struct TestFunctor
 {
     int operator()(int i, double d)
@@ -10,14 +13,49 @@ struct TestFunctor
     }
 };
 
-TEST(TestFunctorBaseFunctionality, shallSuccessfullyCreateAndExecuteFunctorsBracketOperator)
+class TestFunctorBaseFunctionality : public Test
+{
+protected:
+    static constexpr auto input1{10};
+    static constexpr auto input2{2.5};
+    static constexpr auto expected{25};
+};
+
+TEST_F(TestFunctorBaseFunctionality, shallCallFunctorWithFunctorsBracketOperator)
 {
     //given
     TestFunctor f;
-    constexpr auto input1{10};
-    constexpr auto input2{2.5};
-    constexpr auto expected{25};
     Functor<int, TYPELIST_2(int, double)> cmd(f);
     //when & then
     ASSERT_EQ(expected, cmd(input1, input2));
+}
+
+int testFunction(int i, double d)
+{
+    return i * d;
+}
+
+TEST_F(TestFunctorBaseFunctionality, shallCallFunction)
+{
+    //given
+    Functor<int, TYPELIST_2(int, double)> cmd(&testFunction);
+    //when & then
+    ASSERT_EQ(expected, cmd(input1, input2));
+}
+
+int testFunction(int i)
+{
+    return i;
+}
+
+TEST_F(TestFunctorBaseFunctionality, shallCallFunctionOverload)
+{
+    //given
+    using Fun = int (*)(int);
+    Functor<int, TYPELIST_1(int)> cmd(static_cast<Fun>(&testFunction));
+    //when & then
+    ASSERT_EQ(input1, cmd(input1));
+}
+
+
 }
